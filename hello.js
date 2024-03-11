b17a0fc957caeb84b0902c74a12d8af4ddb9a246
bchazelle <bchaz@protonmail.com>
2024-03-11 18:57:53 -0500

    Edit Hello world

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log('Hello! My name is Brandon. I am 35 years old.');
+console.log('Hello, world!');
 
 /*   
 Next, PLAY! 
